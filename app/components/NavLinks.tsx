fe88ef106ebd215dc23d73f47d6729cc3c2e1fae
Edwin Martinson <edwinotumartinson@outlook.com>
2025-06-07 19:12:16 +0000

    feat: add tooltip component and integrate with NavLinks for enhanced user experience

--- app/components/NavLinks.tsx
+++ app/components/NavLinks.tsx
@@ -1,20 +1,21 @@
 import type React from "react";
 import { href, NavLink, useNavigation } from "react-router";
 import { cn } from "~/lib/utils";
+import { TooltipContent, Tooltip, TooltipTrigger } from "./ui/tooltip";
 
 export default function NavLinks() {
   return (
     <div className="flex items-center gap-2">
-      <CircleLink link={href("/")}>
+      <CircleLink link={href("/")} label="Dexie">
         <p>🏚️</p>
       </CircleLink>
-      <CircleLink link={href("/redux")}>
+      <CircleLink link={href("/redux")} label="Redux">
         <p>⚛️</p>
       </CircleLink>
-      <CircleLink link={href("/zustand")}>
+      <CircleLink link={href("/zustand")} label="Zustand">
         <p>🐻</p>
       </CircleLink>
-      <CircleLink link={href("/jotai")}>
+      <CircleLink link={href("/jotai")} label="Jotai">
         <p>👻</p>
       </CircleLink>
     </div>
@@ -23,24 +24,32 @@ export default function NavLinks() {
 
 type NavLinkProps = {
   link: string;
+  label: string;
   children: React.ReactNode;
 };
 
-function CircleLink({ link, children }: NavLinkProps) {
+function CircleLink({ link, children, label }: NavLinkProps) {
   const { location, state } = useNavigation();
   const isActive = location?.pathname === link;
 
   return (
     <NavLink to={link}>
-      <div
-        className={cn(
-          "bg-surface-secondary ring-surface-secondary hover:ring-surface-tertiary flex size-10 items-center justify-center rounded-full ring-1 transition-all duration-150 ease-in-out hover:scale-115",
-          isActive && "ring-accent-blue",
-          state === "loading" && "animate-pulse",
-        )}
-      >
-        {children}
-      </div>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <div
+            className={cn(
+              "bg-surface-secondary ring-surface-secondary hover:ring-surface-tertiary flex size-10 items-center justify-center rounded-full ring-1 transition-all duration-150 ease-in-out hover:scale-115",
+              isActive && "ring-accent-blue",
+              state === "loading" && "animate-pulse",
+            )}
+          >
+            {children}
+          </div>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>{label}</p>
+        </TooltipContent>
+      </Tooltip>
     </NavLink>
   );
 }
